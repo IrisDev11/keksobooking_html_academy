d122853b0451050f8e7692ca1f1285603795b31b
IrisDev11 <iris@dostavista.ru>
2022-11-20 21:06:33 +0300

    init

--- source/js/map.js
+++ source/js/map.js
@@ -40,14 +40,14 @@ const renderMap = () => {
 
 //главная метка
 const mainPinIcon = MAP_LIB.icon({
-  iconUrl: '../img/main-pin.svg',
+  iconUrl: './img/main-pin.svg',
   iconSize: [52, 52],
   iconAnchor: [26, 52],
 });
 
 //метка объявлений
 const pinAdIcon = MAP_LIB.icon({
-  iconUrl: '../img/pin.svg',
+  iconUrl: './img/pin.svg',
   iconSize: [40, 40],
   iconAnchor: [20, 40],
 });
